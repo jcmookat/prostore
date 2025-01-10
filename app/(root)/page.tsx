0d4fd680a1ff91df54f6e7e1326533784061f77c
Jose Cardenas <jscardenas717@rocketmail.com>
2025-01-10 21:38:54 +0900

    Sample Products & Product List

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,14 @@
+import ProductList from '@/components/shared/product/product-list';
+import sampleData from '@/db/sample-data';
 const Homepage = () => {
-	return <div>Home</div>;
+	return (
+		<>
+			<ProductList
+				data={sampleData.products}
+				title='Newest Arrivals'
+				limit={4}
+			/>
+		</>
+	);
 };
 export default Homepage;
