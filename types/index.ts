0a4139363a08222d5a5f0dd8860b42154345fd53
Jose Cardenas <jscardenas717@rocketmail.com>
2025-01-30 12:08:43 +0900

    Cart Zod Schema and Prisma Model

--- types/index.ts
+++ types/index.ts
@@ -1,13 +1,11 @@
 import { z } from 'zod';
-import { insertProductSchema } from '@/lib/validators';
+import {
+	insertProductSchema,
+	insertCartSchema,
+	cartItemSchema,
+} from '@/lib/validators';
 import { signUpFormSchema } from '@/lib/validators';
 import { FieldPath, Control } from 'react-hook-form';
-
-export type Product = z.infer<typeof insertProductSchema> & {
-	id: string; // not inserted but created automatically in the DB
-	rating: string;
-	createdAt: Date; // not inserted but created automatically in the DB
-};
 
 export type SignupFormFieldProps = {
 	name: FieldPath<z.infer<typeof signUpFormSchema>>;
@@ -17,3 +15,13 @@ export type SignupFormFieldProps = {
 	inputType?: string;
 	formControl: Control<z.infer<typeof signUpFormSchema>, any>;
 };
+
+export type Product = z.infer<typeof insertProductSchema> & {
+	id: string; // not inserted but created automatically in the DB
+	rating: string;
+	createdAt: Date; // not inserted but created automatically in the DB
+};
+
+export type Cart = z.infer<typeof insertCartSchema>;
+
+export type CartItem = z.infer<typeof cartItemSchema>;
