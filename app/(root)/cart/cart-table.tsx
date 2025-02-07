5d94c53b143601699cd3d7d9145b3e6157c6734a
Jose Cardenas <jscardenas717@rocketmail.com>
2025-02-07 15:44:53 +0900

    Subtotal Cart

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -18,6 +18,8 @@ import {
 } from '@/components/ui/table';
 import { Button } from '@/components/ui/button';
 import { formatNumberWithDecimal } from '@/lib/utils';
+import { Card, CardContent } from '@/components/ui/card';
+import { formatCurrency } from '@/lib/utils';
 
 const CartTable = ({ cart }: { cart?: Cart }) => {
 	const router = useRouter();
@@ -112,6 +114,30 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
 							</TableBody>
 						</Table>
 					</div>
+					<Card>
+						<CardContent className='p-4 gap-4'>
+							<div className='pb-3 text-xl'>
+								Subtotal ({cart.items.reduce((acc, curr) => acc + curr.qty, 0)}
+								):
+								<span className='font-bold'>
+									{formatCurrency(cart.itemsPrice)}
+								</span>
+							</div>
+							<Button
+								className='w-full'
+								disabled={isPending}
+								onClick={() =>
+									startTransition(() => router.push('/shipping-address'))
+								}>
+								{isPending ? (
+									<Loader className='w-4 h-4 animate-spin' />
+								) : (
+									<ArrowRight className='w-4 h-4' />
+								)}
+								Proceed to Checkout
+							</Button>
+						</CardContent>
+					</Card>
 				</div>
 			)}
 		</>
