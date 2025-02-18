b3b465705abe8aa5a5e7d6e293f49e2aac8011f9
Jose Cardenas <jscardenas717@rocketmail.com>
2025-02-18 22:47:02 +0900

    Get My Orders Action

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,4 +1,4 @@
-'use server';
+('use server');
 
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
 import { convertToPlainObject, formatError } from '../utils';
@@ -10,6 +10,7 @@ import { prisma } from '@/db/prisma';
 import { CartItem, PaymentResult } from '@/types';
 import { paypal } from '../paypal';
 import { revalidatePath } from 'next/cache';
+import { PAGE_SIZE } from '../constants';
 
 // Create order and create the order items
 export async function createOrder() {
@@ -277,4 +278,32 @@ async function updateOrderToPaid({
 	if (!updatedOrder) {
 		throw new Error('Order not found');
 	}
+}
+
+// Get user's orders
+export async function getMyOrders({
+	limit = PAGE_SIZE,
+	page,
+}: {
+	limit?: number;
+	page: number;
+}) {
+	const session = await auth();
+	if (!session) throw new Error('User is not authenticated');
+
+	const data = await prisma.order.findMany({
+		where: { userId: session?.user?.id! },
+		orderBy: { createdAt: 'desc' },
+		take: limit,
+		skip: (page - 1) * limit, // for pagination, for example, you have 6 total orders and you are in the second page, you wanna skip the first 3
+	});
+
+	const dataCount = await prisma.order.count({
+		where: { userId: session?.user?.id! },
+	});
+
+	return {
+		data,
+		totalPages: Math.ceil(dataCount / limit),
+	};
 }
