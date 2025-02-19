80b0e37b170a94e0ebb0c77353dbf7b5a5f38344
Jose Cardenas <jscardenas717@rocketmail.com>
2025-02-20 08:58:31 +0900

    order actions ts error

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -292,14 +292,14 @@ export async function getMyOrders({
 	if (!session) throw new Error('User is not authenticated');
 
 	const data = await prisma.order.findMany({
-		where: { userId: session?.user?.id! },
+		where: { userId: session.user.id! },
 		orderBy: { createdAt: 'desc' },
 		take: limit,
 		skip: (page - 1) * limit, // for pagination, for example, you have 6 total orders and you are in the second page, you wanna skip the first 3
 	});
 
 	const dataCount = await prisma.order.count({
-		where: { userId: session?.user?.id! },
+		where: { userId: session.user.id! },
 	});
 
 	return {
