aefe25b6a71f05200be5847b5f371ec300085ad5
Jose Cardenas <jscardenas717@rocketmail.com>
2025-02-20 09:54:40 +0900

    redirect if they don't own order

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -1,8 +1,9 @@
 import { Metadata } from 'next';
 import { getOrderById } from '@/lib/actions/order.actions';
-import { notFound } from 'next/navigation';
+import { notFound, redirect } from 'next/navigation';
 import OrderDetailsTable from './order-details-table';
 import { ShippingAddress } from '@/types';
+import { auth } from '@/auth';
 
 export const metadata: Metadata = {
 	title: 'Order Details',
@@ -13,10 +14,20 @@ const OrderDetailsPage = async (props: {
 		id: string;
 	}>;
 }) => {
+	// Get order ID
 	const { id } = await props.params;
-
 	const order = await getOrderById(id);
 	if (!order) notFound();
+
+	// Get session
+	const session = await auth();
+	const userId = session?.user?.id;
+	if (!userId) throw new Error('No User ID');
+
+	// Redirect the user if they don't own the order
+	if (order.userId !== session?.user?.id) {
+		return redirect('/unauthorized');
+	}
 
 	return (
 		<>
