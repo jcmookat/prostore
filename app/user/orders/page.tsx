a75d88207d07b0e440027c4dea280e6487c335e9
Jose Cardenas <jscardenas717@rocketmail.com>
2025-02-22 08:47:19 +0900

    prisma update and nvim test

--- app/user/orders/page.tsx
+++ app/user/orders/page.tsx
@@ -1,86 +1,85 @@
-import { Metadata } from 'next';
-import { getMyOrders } from '@/lib/actions/order.actions';
-import { formatCurrency, formatDateTime, formatId } from '@/lib/utils';
-import Link from 'next/link';
+import { Metadata } from "next";
+import { getMyOrders } from "@/lib/actions/order.actions";
+import { formatCurrency, formatDateTime, formatId } from "@/lib/utils";
+import Link from "next/link";
 import {
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
-} from '@/components/ui/table';
-import { Badge } from '@/components/ui/badge';
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Badge } from "@/components/ui/badge";
 
 export const metadata: Metadata = {
-	title: 'My Orders',
+  title: "My Orders",
 };
 
 const OrdersPage = async (props: {
-	searchParams: Promise<{ page: string }>;
+  searchParams: Promise<{ page: string }>;
 }) => {
-	const { page } = await props.searchParams;
+  const { page } = await props.searchParams;
 
-	const orders = await getMyOrders({
-		page: Number(page) || 1,
-	});
+  const orders = await getMyOrders({
+    page: Number(page) || 1,
+  });
 
-	return (
-		<div className='space-y-2'>
-			<h2 className='h2-bold'>Orders</h2>
-			<div className='overflow-x-auto'>
-				<Table>
-					<TableHeader>
-						<TableRow>
-							<TableHead>ID</TableHead>
-							<TableHead>DATE</TableHead>
-							<TableHead>TOTAL</TableHead>
-							<TableHead>PAID</TableHead>
-							<TableHead>DELIVERED</TableHead>
-							<TableHead>ACTIONS</TableHead>
-						</TableRow>
-					</TableHeader>
-					<TableBody>
-						{orders.data.map((order) => (
-							<TableRow key={order.id}>
-								<TableCell>{formatId(order.id)}</TableCell>
-								<TableCell>
-									{formatDateTime(order.createdAt).dateTime}
-								</TableCell>
-								<TableCell>{formatCurrency(order.totalPrice)}</TableCell>
-								<TableCell>
-									{order.isPaid && order.paidAt ? (
-										<>
-											<Badge variant='default'>Paid</Badge>{' '}
-											{formatDateTime(order.paidAt).dateTime}
-										</>
-									) : (
-										<Badge variant='destructive'>Not paid</Badge>
-									)}
-								</TableCell>
-								<TableCell>
-									{order.isDelivered && order.deliveredAt ? (
-										<>
-											<Badge variant='default'>Delivered</Badge>{' '}
-											{formatDateTime(order.deliveredAt).dateTime}
-										</>
-									) : (
-										<Badge variant='destructive'>Not delivered</Badge>
-									)}
-								</TableCell>
-
-								<TableCell>
-									<Link href={`/order/${order.id}`}>
-										<span className='px-2'>Details</span>
-									</Link>
-								</TableCell>
-							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-			</div>
-		</div>
-	);
+  return (
+    <div className="space-y-2">
+      <h2 className="h2-bold">Orders</h2>
+      <div className="overflow-x-auto">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>DATE</TableHead>
+              <TableHead>TOTAL</TableHead>
+              <TableHead>PAID</TableHead>
+              <TableHead>DELIVERED</TableHead>
+              <TableHead>ACTIONS</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {orders.data.map((order) => (
+              <TableRow key={order.id}>
+                <TableCell>{formatId(order.id)}</TableCell>
+                <TableCell>
+                  {formatDateTime(order.createdAt).dateTime}
+                </TableCell>
+                <TableCell>{formatCurrency(order.totalPrice)}</TableCell>
+                <TableCell>
+                  {order.isPaid && order.paidAt ? (
+                    <>
+                      <Badge variant="default">Paid</Badge>{" "}
+                      {formatDateTime(order.paidAt).dateTime}
+                    </>
+                  ) : (
+                    <Badge variant="destructive">Not paid</Badge>
+                  )}
+                </TableCell>
+                <TableCell>
+                  {order.isDelivered && order.deliveredAt ? (
+                    <>
+                      <Badge variant="default">Delivered</Badge>{" "}
+                      {formatDateTime(order.deliveredAt).dateTime}
+                    </>
+                  ) : (
+                    <Badge variant="destructive">Not delivered</Badge>
+                  )}
+                </TableCell>
+                <TableCell>
+                  <Link href={`/order/${order.id}`}>
+                    <span className="px-2">Details</span>
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
 };
 
 export default OrdersPage;
