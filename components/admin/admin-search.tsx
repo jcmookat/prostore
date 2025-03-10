3b3dd58efe1595092dea2791469f21b1e3280cf4
Jose Cardenas <jscardenas717@rocketmail.com>
2025-03-10 16:03:59 +0900

    Admin seach form, query, added debounce

--- components/admin/admin-search.tsx
+++ components/admin/admin-search.tsx
@@ -1,15 +1,18 @@
 'use client';
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 import { useEffect, useState, type ReactElement } from 'react';
 import { Input } from '../ui/input';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function AdminSearch(): ReactElement {
   const pathname = usePathname();
-  const formActionUrl = pathname.includes('/admin/orders')
-    ? '/admin/orders'
-    : pathname.includes('/admin/users')
-      ? '/admin/users'
-      : '/admin/products';
+  const { replace } = useRouter();
+
+  // const formActionUrl = pathname.includes('/admin/orders')
+  //   ? '/admin/orders'
+  //   : pathname.includes('/admin/users')
+  //     ? '/admin/users'
+  //     : '/admin/products';
 
   const searchParams = useSearchParams();
   const [queryValue, setQueryValue] = useState(searchParams.get('query') || '');
@@ -17,19 +20,48 @@ export default function AdminSearch(): ReactElement {
   useEffect(() => {
     setQueryValue(searchParams.get('query') || '');
   }, [searchParams]);
+
+  // const handleSearch = useDebouncedCallback(
+  //   (e: React.ChangeEvent<HTMLInputElement>) => {
+  //     const newQuery = e.target.value;
+  //     setQueryValue(newQuery);
+  //
+  //     const params = new URLSearchParams(searchParams);
+  //     if (newQuery) {
+  //       params.set('query', newQuery);
+  //     } else {
+  //       params.delete('query');
+  //     }
+  //
+  //     replace(`${pathname}?${params.toString()}`);
+  //   },
+  //   300,
+  // );
+  const updateSearchParams = useDebouncedCallback((query: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (query) {
+      params.set('query', query);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newQuery = e.target.value;
+    setQueryValue(newQuery);
+    updateSearchParams(newQuery);
+  };
   return (
-    <form action={formActionUrl} method="GET">
+    <>
       <Input
         type="search"
         placeholder="Search..."
         name="query"
         value={queryValue}
-        onChange={(e) => setQueryValue(e.target.value)}
+        onChange={handleSearch}
         className="md:w-[100px] lg:w-[300px]"
       />
-      <button type="submit" className="sr-only">
-        Search
-      </button>
-    </form>
+    </>
   );
 }
