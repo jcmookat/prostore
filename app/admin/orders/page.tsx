48f6d894ee5a84168d32b7b41af5c277f5ccd6aa
Jose Cardenas <jscardenas717@rocketmail.com>
2025-03-10 22:24:37 +0900

    products, orders, and users search by admin

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -3,12 +3,12 @@ import { deleteOrder, getAllOrders } from '@/lib/actions/order.actions';
 import { requireAdmin } from '@/lib/auth-guard';
 import { Metadata } from 'next';
 import {
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
 } from '@/components/ui/table';
 import { formatCurrency, formatDateTime, formatId } from '@/lib/utils';
 import Link from 'next/link';
@@ -18,88 +18,107 @@ import { Button } from '@/components/ui/button';
 import DeleteDialog from '@/components/shared/delete-dialog';
 
 export const metadata: Metadata = {
-	title: 'Admin Orders',
+  title: 'Admin Orders',
 };
 export default async function AdminOrdersPage(props: {
-	searchParams?: Promise<{ page?: string }>;
+  searchParams?: Promise<{
+    page?: string;
+    query?: string;
+  }>;
 }): Promise<ReactElement> {
-	await requireAdmin();
-	const searchParams = await props.searchParams;
-	const page = Number(searchParams?.page) || 1;
-	const orders = await getAllOrders({
-		page,
-	});
-	const { data, totalPages } = orders;
+  await requireAdmin();
+  const searchParams = await props.searchParams;
+  const page = Number(searchParams?.page) || 1;
+  const searchText = searchParams?.query || '';
+  const orders = await getAllOrders({
+    page,
+    query: searchText,
+  });
+  const { data, totalPages } = orders;
 
-	return (
-		<div className='space-y-2'>
-			<h2 className='h2-bold'>Orders</h2>
-			<div className='overflow-x-auto'>
-				<Table>
-					<TableHeader>
-						<TableRow>
-							<TableHead>ID</TableHead>
-							<TableHead>DATE</TableHead>
-							<TableHead>Buyer</TableHead>
-							<TableHead>TOTAL</TableHead>
-							<TableHead>PAYMENT METHOD</TableHead>
-							<TableHead>PAID</TableHead>
-							<TableHead>DELIVERED</TableHead>
-							<TableHead>ACTIONS</TableHead>
-						</TableRow>
-					</TableHeader>
-					<TableBody>
-						{data.map((order) => (
-							<TableRow key={order.id}>
-								<TableCell>{formatId(order.id)}</TableCell>
-								<TableCell>
-									{formatDateTime(order.createdAt).dateTime}
-								</TableCell>
-								<TableCell>{order.user.name}</TableCell>
-								<TableCell>{formatCurrency(order.totalPrice)}</TableCell>
-								<TableCell>{order.paymentMethod}</TableCell>
-								<TableCell>
-									{order.isPaid && order.paidAt ? (
-										<>
-											<Badge variant='default' className='mr-1'>
-												Paid
-											</Badge>{' '}
-											{formatDateTime(order.paidAt).dateTime}
-										</>
-									) : (
-										<Badge variant='secondary'>Not paid</Badge>
-									)}
-								</TableCell>
-								<TableCell>
-									{order.isDelivered && order.deliveredAt ? (
-										<>
-											<Badge variant='default' className='mr-1'>
-												Delivered
-											</Badge>{' '}
-											{formatDateTime(order.deliveredAt).dateTime}
-										</>
-									) : (
-										<Badge variant='secondary'>Not delivered</Badge>
-									)}
-								</TableCell>
-								<TableCell>
-									<Button asChild variant='outline' size='sm'>
-										<Link href={`/order/${order.id}`}>Details</Link>
-									</Button>
-									<DeleteDialog id={order.id} action={deleteOrder} />
-								</TableCell>
-							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-				{totalPages > 1 && (
-					<Pagination
-						page={page}
-						totalPages={totalPages}
-						pathName='/admin/orders'
-					/>
-				)}
-			</div>
-		</div>
-	);
+  return (
+    <div className="space-y-2">
+      <div className="flex items-center gap-4">
+        <h1 className="h2-bold">Orders</h1>
+        {searchText && (
+          <div>
+            {' '}
+            Filtered by{' '}
+            <i className="mr-4 font-bold">&quot;{searchText}&quot;</i>
+            <Link href={`/admin/orders`}>
+              <Button variant="outline" size="sm">
+                Remove Filter
+              </Button>
+            </Link>
+          </div>
+        )}
+      </div>
+      <div className="overflow-x-auto">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>DATE</TableHead>
+              <TableHead>Buyer</TableHead>
+              <TableHead>TOTAL</TableHead>
+              <TableHead>PAYMENT METHOD</TableHead>
+              <TableHead>PAID</TableHead>
+              <TableHead>DELIVERED</TableHead>
+              <TableHead>ACTIONS</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data.map((order) => (
+              <TableRow key={order.id}>
+                <TableCell>{formatId(order.id)}</TableCell>
+                <TableCell>
+                  {formatDateTime(order.createdAt).dateTime}
+                </TableCell>
+                <TableCell>{order.user.name}</TableCell>
+                <TableCell>{formatCurrency(order.totalPrice)}</TableCell>
+                <TableCell>{order.paymentMethod}</TableCell>
+                <TableCell>
+                  {order.isPaid && order.paidAt ? (
+                    <>
+                      <Badge variant="default" className="mr-1">
+                        Paid
+                      </Badge>{' '}
+                      {formatDateTime(order.paidAt).dateTime}
+                    </>
+                  ) : (
+                    <Badge variant="secondary">Not paid</Badge>
+                  )}
+                </TableCell>
+                <TableCell>
+                  {order.isDelivered && order.deliveredAt ? (
+                    <>
+                      <Badge variant="default" className="mr-1">
+                        Delivered
+                      </Badge>{' '}
+                      {formatDateTime(order.deliveredAt).dateTime}
+                    </>
+                  ) : (
+                    <Badge variant="secondary">Not delivered</Badge>
+                  )}
+                </TableCell>
+                <TableCell>
+                  <Button asChild variant="outline" size="sm">
+                    <Link href={`/order/${order.id}`}>Details</Link>
+                  </Button>
+                  <DeleteDialog id={order.id} action={deleteOrder} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        {totalPages > 1 && (
+          <Pagination
+            page={page}
+            totalPages={totalPages}
+            pathName="/admin/orders"
+          />
+        )}
+      </div>
+    </div>
+  );
 }
--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -1,11 +1,11 @@
 import { type ReactElement } from 'react';
 import {
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
 } from '@/components/ui/table';
 import { Metadata } from 'next';
 import { deleteUser, getAllUsers } from '@/lib/actions/user.actions';
@@ -18,69 +18,84 @@ import { Badge } from '@/components/ui/badge';
 import { requireAdmin } from '@/lib/auth-guard';
 
 export const metadata: Metadata = {
-	title: 'Users',
+  title: 'Users',
 };
 
 export default async function AdminUserPage(props: {
-	searchParams?: Promise<{
-		page?: string;
-	}>;
+  searchParams?: Promise<{
+    page?: string;
+    query?: string;
+  }>;
 }): Promise<ReactElement> {
-	await requireAdmin();
-	const searchParams = await props.searchParams;
-	const page = Number(searchParams?.page) || 1;
+  await requireAdmin();
+  const searchParams = await props.searchParams;
+  const page = Number(searchParams?.page) || 1;
+  const searchText = searchParams?.query || '';
 
-	const users = await getAllUsers({
-		page,
-	});
-	const { data, totalPages } = users;
-	return (
-		<div className='space-y-2'>
-			<div className='flex-between'>
-				<h1 className='h2-bold'>Users</h1>
-			</div>
-			<div>
-				<Table>
-					<TableHeader>
-						<TableRow>
-							<TableHead>ID</TableHead>
-							<TableHead>NAME</TableHead>
-							<TableHead>EMAIL</TableHead>
-							<TableHead>ROLE</TableHead>
-							<TableHead className='w-[100px]'>ACTIONS</TableHead>
-						</TableRow>
-					</TableHeader>
-					<TableBody>
-						{data.map((user) => (
-							<TableRow key={user.id}>
-								<TableCell>{formatId(user.id)}</TableCell>
-								<TableCell>{user.name}</TableCell>
-								<TableCell>{user.email}</TableCell>
-								<TableCell>
-									{user.role === 'user' ? (
-										<Badge variant='secondary'>User</Badge>
-									) : (
-										<Badge variant='default'>Admin</Badge>
-									)}
-								</TableCell>
-								<TableCell className='flex gap-1'>
-									<Button asChild variant='outline' size='sm'>
-										<Link href={`/admin/users/${user.id}`}>Edit</Link>
-									</Button>
-									<DeleteDialog id={user.id} action={deleteUser} />
-								</TableCell>
-							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-				{totalPages > 1 && (
-					<Pagination
-						page={page}
-						totalPages={totalPages}
-						pathName='/admin/users'
-					/>
-				)}
-			</div>
-		</div>
-	);
+  const users = await getAllUsers({
+    page,
+    query: searchText,
+  });
+  const { data, totalPages } = users;
+  return (
+    <div className="space-y-2">
+      <div className="flex items-center gap-4">
+        <h1 className="h2-bold">Orders</h1>
+        {searchText && (
+          <div>
+            {' '}
+            Filtered by{' '}
+            <i className="mr-4 font-bold">&quot;{searchText}&quot;</i>
+            <Link href={`/admin/orders`}>
+              <Button variant="outline" size="sm">
+                Remove Filter
+              </Button>
+            </Link>
+          </div>
+        )}
+      </div>
+      <div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>NAME</TableHead>
+              <TableHead>EMAIL</TableHead>
+              <TableHead>ROLE</TableHead>
+              <TableHead className="w-[100px]">ACTIONS</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell>{formatId(user.id)}</TableCell>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>
+                  {user.role === 'user' ? (
+                    <Badge variant="secondary">User</Badge>
+                  ) : (
+                    <Badge variant="default">Admin</Badge>
+                  )}
+                </TableCell>
+                <TableCell className="flex gap-1">
+                  <Button asChild variant="outline" size="sm">
+                    <Link href={`/admin/users/${user.id}`}>Edit</Link>
+                  </Button>
+                  <DeleteDialog id={user.id} action={deleteUser} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        {totalPages > 1 && (
+          <Pagination
+            page={page}
+            totalPages={totalPages}
+            pathName="/admin/users"
+          />
+        )}
+      </div>
+    </div>
+  );
 }
