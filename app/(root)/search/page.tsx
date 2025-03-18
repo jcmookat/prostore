894c2d86c1ea5fd6e4ae98417eee203efcc92a00
Jose Cardenas <jscardenas717@rocketmail.com>
2025-03-18 22:05:31 +0900

    Get filter URL function

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -4,45 +4,69 @@ import { getAllProducts } from '@/lib/actions/product.actions';
 import { type ReactElement } from 'react';
 
 export default async function SearchPage(props: {
-	searchParams?: Promise<{
-		q?: string;
-		category?: string;
-		price?: string;
-		rating?: string;
-		sort?: string;
-		page?: string;
-	}>;
+  searchParams?: Promise<{
+    q?: string;
+    category?: string;
+    price?: string;
+    rating?: string;
+    sort?: string;
+    page?: string;
+  }>;
 }): Promise<ReactElement> {
-	const searchParams = (await props.searchParams) ?? {};
-	const {
-		q = 'all',
-		category = 'all',
-		price = 'all',
-		rating = 'all',
-		sort = 'newest',
-		page = '1',
-	} = searchParams;
+  const searchParams = (await props.searchParams) ?? {};
+  const {
+    q = 'all',
+    category = 'all',
+    price = 'all',
+    rating = 'all',
+    sort = 'newest',
+    page = '1',
+  } = searchParams;
 
-	const { data: products, totalPages } = await getAllProducts({
-		query: q,
-		category,
-		price,
-		rating,
-		sort,
-		page: Number(page),
-	});
-	return (
-		<div className='grid md:grid-cols-5 md:gap-5'>
-			<div className='filter-links'>Filters</div>
-			<div className='md:col-span-4 space-y-4'>
-				<div className='grid grid-cols-1 gap-4 md:grid-cols-3'>
-					{products.length === 0 && <div>No product found</div>}
-					{products.map((product) => (
-						<ProductCard key={product.id} product={product} />
-					))}
-					{totalPages > 1 && <Pagination page={page} totalPages={totalPages} />}
-				</div>
-			</div>
-		</div>
-	);
+  // Construct filter URL
+  const getFilterUrl = ({
+    c,
+    p,
+    r,
+    s,
+    pg,
+  }: {
+    c?: string;
+    p?: string;
+    r?: string;
+    s?: string;
+    pg?: string;
+  }) => {
+    const params = { q, category, price, rating, sort, page };
+    if (c) params.category = c;
+    if (p) params.price = p;
+    if (r) params.rating = r;
+    if (s) params.sort = s;
+    if (pg) params.page = pg;
+
+    return `/search?${new URLSearchParams(params).toString()}`;
+  };
+
+  const { data: products, totalPages } = await getAllProducts({
+    query: q,
+    category,
+    price,
+    rating,
+    sort,
+    page: Number(page),
+  });
+  return (
+    <div className="grid md:grid-cols-5 md:gap-5">
+      <div className="filter-links">Filters</div>
+      <div className="md:col-span-4 space-y-4">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+          {products.length === 0 && <div>No product found</div>}
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+          {totalPages > 1 && <Pagination page={page} totalPages={totalPages} />}
+        </div>
+      </div>
+    </div>
+  );
 }
