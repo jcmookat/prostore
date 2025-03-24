3c00bfe5191b40e1cce8a898db09b7d5255435de
Jose Cardenas <jscardenas717@rocketmail.com>
2025-03-24 15:25:20 +0900

    Stripe Payment Component

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -7,58 +7,58 @@ import { auth } from '@/auth';
 import Stripe from 'stripe';
 
 export const metadata: Metadata = {
-	title: 'Order Details',
+  title: 'Order Details',
 };
 
 const OrderDetailsPage = async (props: {
-	params: Promise<{
-		id: string;
-	}>;
+  params: Promise<{
+    id: string;
+  }>;
 }) => {
-	// Get order ID
-	const { id } = await props.params;
-	const order = await getOrderById(id);
+  // Get order ID
+  const { id } = await props.params;
+  const order = await getOrderById(id);
 
-	// Get session
-	const session = await auth();
-	const userId = session?.user?.id;
-	const userRole = session?.user?.role;
+  // Get session
+  const session = await auth();
+  const userId = session?.user?.id;
+  const userRole = session?.user?.role;
 
-	if (!userId) throw new Error('No User ID');
+  if (!userId) throw new Error('No User ID');
 
-	// Redirect the user if they don't own the order
-	if (order.userId !== userId && userRole !== 'admin') {
-		return redirect('/unauthorized');
-	}
+  // Redirect the user if they don't own the order
+  if (order.userId !== userId && userRole !== 'admin') {
+    return redirect('/unauthorized');
+  }
 
-	let client_secret = null;
+  let client_secret = null;
 
-	// Check if using Stripe and not paid
-	if (order.paymentMethod === 'stripe' && !order.isPaid) {
-		const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
-		const paymentIntent = await stripe.paymentIntents.create({
-			amount: Math.round(Number(order.totalPrice) * 100),
-			currency: 'USD',
-			metadata: {
-				orderId: order.id,
-			},
-		});
-		client_secret = paymentIntent.client_secret;
-	}
+  // Check if using Stripe and not paid
+  if (order.paymentMethod === 'Stripe' && !order.isPaid) {
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(Number(order.totalPrice) * 100),
+      currency: 'USD',
+      metadata: {
+        orderId: order.id,
+      },
+    });
+    client_secret = paymentIntent.client_secret;
+  }
 
-	return (
-		<>
-			<OrderDetailsTable
-				order={{
-					...order,
-					shippingAddress: order.shippingAddress as ShippingAddress,
-				}}
-				stripeClientSecret={client_secret}
-				paypalClientId={process.env.PAYPAL_CLIENT_ID || 'sb'}
-				isAdmin={userRole === 'admin' || false}
-			/>
-		</>
-	);
+  return (
+    <>
+      <OrderDetailsTable
+        order={{
+          ...order,
+          shippingAddress: order.shippingAddress as ShippingAddress,
+        }}
+        stripeClientSecret={client_secret}
+        paypalClientId={process.env.PAYPAL_CLIENT_ID || 'sb'}
+        isAdmin={userRole === 'admin' || false}
+      />
+    </>
+  );
 };
 
 export default OrderDetailsPage;
