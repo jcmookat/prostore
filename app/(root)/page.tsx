6adb3bd93591608e389a7674bd89fe87267282d1
Jose Cardenas <jscardenas717@rocketmail.com>
2025-03-29 14:31:24 +0900

    icon box, deal countdown

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,26 @@
 import ProductList from '@/components/shared/product/product-list';
 import {
-	getLatestProducts,
-	getFeaturedProducts,
+  getLatestProducts,
+  getFeaturedProducts,
 } from '@/lib/actions/product.actions';
 import ProductCarousel from '@/components/shared/product/product-carousel';
 import ViewAllProductsButton from '@/components/view-all-products';
+import IconBoxes from '@/components/icon-boxex';
+import { DealCountdown } from '@/components/deal-countdown';
 
 const Homepage = async () => {
-	const latestProducts = await getLatestProducts();
-	const featuredProducts = await getFeaturedProducts();
-	return (
-		<>
-			{featuredProducts.length > 0 && (
-				<ProductCarousel data={featuredProducts} />
-			)}
-			<ProductList data={latestProducts} title='Newest Arrivals' limit={4} />
-			<ViewAllProductsButton />
-		</>
-	);
+  const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
+  return (
+    <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
+      <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
+      <ViewAllProductsButton />
+      <DealCountdown />
+      <IconBoxes />
+    </>
+  );
 };
 export default Homepage;
